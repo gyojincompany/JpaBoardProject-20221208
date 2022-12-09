89ecd58a81dfb8b6f49b07bdc73307a874c5c568
gyojincompany <luckyred@gmail.com>
2022-12-09 12:21:38 +0900

    질문 내용 상세 보기

--- src/main/java/com/gyojincompany/gyojinboard/service/QuestionService.java
+++ src/main/java/com/gyojincompany/gyojinboard/service/QuestionService.java
@@ -1,12 +1,14 @@
 package com.gyojincompany.gyojinboard.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gyojincompany.gyojinboard.dto.QuestionDto;
 import com.gyojincompany.gyojinboard.entity.Question;
+import com.gyojincompany.gyojinboard.exception.DataNotFoundException;
 import com.gyojincompany.gyojinboard.repository.AnswerRepository;
 import com.gyojincompany.gyojinboard.repository.QuestionRepository;
 
@@ -26,12 +28,16 @@ public class QuestionService {
 		return questionList;
 	}
 	
-	public Question getQuestion() {
+	public Question getQuestion(Integer id) {
 		
+		Optional<Question> optQuestion = questionRepository.findById(id);
+		if(optQuestion.isPresent()) {
+			Question question = optQuestion.get();
+			return question;
+		} else {
+			throw new DataNotFoundException("해당 질문이 없습니다.");
+		}
 		
-		
-		
-		return question;
 	}
 	
 }
